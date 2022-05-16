faf7a3987459bc43343e046fbb9ed8855a28a835
tanushaveraksa <veraksa161@gmail.com>
2022-05-17 00:20:13 +0300

    feat: add images (May 17, 2022 0:19 PM)

---
+++ cssMemSlider/script.js
@@ -0,0 +1,23 @@
+const mems = [
+    {
+        id: 1,
+        classImg: "",
+        text: ""
+    },
+    {
+        id: 2,
+        classImg: "",
+        text: ""
+    },
+    {
+        id: 3,
+        classImg: "",
+        text: ""
+    },
+    {
+        id: 4,
+        classImg: "",
+        text: ""
+    },
+]
+    
